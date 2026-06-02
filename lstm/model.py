c6ce1016efd673c60fbcc98e6bb326bcda1ad948
Adelaide Gray <grayadelaide23@gmail.com>
2026-06-02 14:25:07 -0700

    edit hidden_size in lstm/model.py

--- lstm/model.py
+++ lstm/model.py
@@ -6,13 +6,13 @@ class TrajectoryPredictor(nn.Module):
         """LSTM trajectory predictor
 
         Layers:
-            LSTM: two stacked LSTMs with input_size=8 for the 8 features. hidden_size=8
+            LSTM: two stacked LSTMs with input_size=8 for the 8 features. hidden_size=64
             Linear: inputs 8 numbers, outputs 24 for 12 predictedsteps * 2 coordinates
         """
         super().__init__()
 
-        self.lstm = nn.LSTM(input_size=8, hidden_size=8, num_layers=2, batch_first=True)
-        self.fc = nn.Linear(8, 24)
+        self.lstm = nn.LSTM(input_size=8, hidden_size=64, num_layers=2, batch_first=True)
+        self.fc = nn.Linear(64, 24)
 
     def forward(self, x):
         """Forward pass
